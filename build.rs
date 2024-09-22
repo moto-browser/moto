5c086551ccd45a187126da6867007f23099f7037
Daniel Adams <msub2official@gmail.com>
2024-09-21 17:16:01 -1000

    Remove webviewer-specific code

--- build.rs
+++ build.rs
@@ -5,8 +5,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::path::Path;
 use std::env;
 
 use gl_generator::{Api, Fallbacks, Profile, Registry};
@@ -20,30 +19,6 @@ fn generate_egl_bindings(out_dir: &Path) {
         .write_bindings(gl_generator::StaticStructGenerator, &mut file)
         .unwrap();
     println!("cargo:rustc-link-lib=EGL");
-}
-
-fn find_python() -> String {
-    env::var("PYTHON3").ok().unwrap_or_else(|| {
-        let candidates = if cfg!(windows) {
-            ["python.exe", "python"]
-        } else {
-            ["python3", "python"]
-        };
-        for &name in &candidates {
-            if Command::new(name)
-                .arg("--version")
-                .output()
-                .ok()
-                .map_or(false, |out| out.status.success())
-            {
-                return name.to_owned();
-            }
-        }
-        panic!(
-            "Can't find python (tried {})! Try fixing PATH or setting the PYTHON3 env var",
-            candidates.join(", ")
-        )
-    })
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
